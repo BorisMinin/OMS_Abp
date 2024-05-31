57027f4f6661d6b16b6a1789f38587c05b41d64d
BMinin <boris.minin@outlook.com>
2024-05-31 18:26:38 +0500

    Some changes

--- src/OMS_Abp.EntityFrameworkCore/EntityFrameworkCore/OMS_AbpDbContext.cs
+++ src/OMS_Abp.EntityFrameworkCore/EntityFrameworkCore/OMS_AbpDbContext.cs
@@ -10,6 +10,7 @@ using Volo.Abp.Identity;
 using Volo.Abp.Identity.EntityFrameworkCore;
 using Volo.Abp.OpenIddict.EntityFrameworkCore;
 using Volo.Abp.PermissionManagement.EntityFrameworkCore;
+using Volo.Abp.SettingManagement;
 using Volo.Abp.SettingManagement.EntityFrameworkCore;
 using Volo.Abp.TenantManagement;
 using Volo.Abp.TenantManagement.EntityFrameworkCore;
@@ -23,22 +24,9 @@ public class OMS_AbpDbContext :
     AbpDbContext<OMS_AbpDbContext>,
     IIdentityDbContext,
     ITenantManagementDbContext
+    //, ISettingManagementDbContext
 {
-    /* Add DbSet properties for your Aggregate Roots / Entities here. */
-
     #region Entities from the modules
-
-    /* Notice: We only implemented IIdentityDbContext and ITenantManagementDbContext
-     * and replaced them for this DbContext. This allows you to perform JOIN
-     * queries for the entities of these modules over the repositories easily. You
-     * typically don't need that for other modules. But, if you need, you can
-     * implement the DbContext interface of the needed module and use ReplaceDbContext
-     * attribute just like IIdentityDbContext and ITenantManagementDbContext.
-     *
-     * More info: Replacing a DbContext of a module ensures that the related module
-     * uses this DbContext on runtime. Otherwise, it will use its own DbContext class.
-     */
-
     //Identity
     public DbSet<IdentityUser> Users { get; set; }
     public DbSet<IdentityRole> Roles { get; set; }
@@ -51,6 +39,10 @@ public class OMS_AbpDbContext :
     // Tenant Management
     public DbSet<Tenant> Tenants { get; set; }
     public DbSet<TenantConnectionString> TenantConnectionStrings { get; set; }
+
+    public DbSet<Setting> Settings { get; set; }
+
+    //public DbSet<SettingDefinitionRecord> SettingDefinitionRecords { get; set; }
 
     #endregion
 
